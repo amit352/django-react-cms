96dd23335d905469855702f470a58b7bc79af1d7
Amit K. Chauhan <kumar352@gmail.com>
2020-07-15 20:37:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ INSTALL_REQUIREMENTS = [
 
 setup(
     name='django-react-cms',
-    version='0.1.0',
+    version='0.1.2',
     author=u'Leonardo Arroyo',
     author_email='contato@leonardoarroyo.com',
     packages=find_packages(),
